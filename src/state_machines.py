0d8a24989b13eaec3dbec08232b7b761215ac0e8
Andrew Ferrouolo <andrew.ferruolo@gmail.com>
2024-07-18 22:49:25 -0400

    Defined Basic agentic behavior

--- src/state_machines.py
+++ src/state_machines.py
@@ -1,37 +1,59 @@
+from typing import List
+
 import pydantic
+
+from behavior_definition import CallCommand
 
 
 class AgentOutput(pydantic.BaseModel):
     success: bool = True
-    agent_id: int
+    agent_id: str
     output: str
 
 
+# This is just a definition to mock up agent behavior and the
+# Agent type and should definitely never ever be used for any real functionality
 class Agent:
-    def __init__(self, agent_id):
-        self.internal_state = ""
+    def __init__(self, agent_id: str, prompt_formatter, fetch_commands: dict, call_before_execute: List[CallCommand],
+                 pass_success_to: str, pass_failure_to: str) -> None:
         self.agent_id = agent_id
+        self.call_before_execute = call_before_execute
+        self.prompt_formatter = prompt_formatter
+        self.fetch_commands = fetch_commands
+        self.pass_success_to = pass_success_to
+        self.pass_failure_to = pass_failure_to
 
-    def run(self, command, data) -> AgentOutput:
+    def run(self, command: str) -> AgentOutput:
         # Creates Infinite Loop, basically just a demo
         # I haven't written a real application in python in months
         return AgentOutput(success=True, agent_id=self.agent_id, output="")
 
+    def format_prompt(self, initial_prompt: str, previous_output: str, fetched_items: List[str]) -> str:
+        return self.prompt_formatter(initial_prompt)
+
+    def fetch_cmd(self, cmd: str) -> str:
+        return cmd
+
 
 class AgentStateMachine:
-    def __init__(self, agents, initial_state: AgentOutput):
-        self.agents = agents
-        self.initial_state: AgentOutput = initial_state
+    def __init__(self, definitions):
+        self.agents = {}  # TODO: Implement Me
 
-    def run_state_machine(self, max_turns):
+    def run_state_machine(self, max_turns: int, start_node: str, task_definition: str):
+        current_node = self.agents[start_node]
+        previous_output = ""
 
-        (agent, command, data) = self.initial_state
-        print("Initial State:")
-        print(f"Agent {agent} executes command {command}")
+        for i in range(max_turns):
+            fetched_data = [self.agents[cmd.agent_name].fetch_cmd(cmd.agent_command) for cmd in
+                            current_node.call_before_execute]
 
-        for i in max_turns:
-            (agent, command, data) = self.agents[agent].run(command, data)
-            print(f"{i}: Agent {agent} executes command {command}")
-            if command == "BREAK":
+            prompt = current_node.format_prompt(fetched_data, previous_output, fetched_data)
+            output: AgentOutput = current_node.run(prompt)
+            previous_output = output.output
+            if output.success:
+                current_node = self.agents[current_node.pass_success_to]
+            else:
+                current_node = self.agents[current_node.pass_failure_to]
+
+            if current_node == "BREAK":
                 break
-        return True
